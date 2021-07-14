4f6faccbbe9dc2781a595a653f09f99b03caff33
raylaw8@hotmail.com <raylaw8@hotmail.com>
2021-07-14 15:12:20 +0000

    week3.1

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,7 +97,14 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "hhttps://b8578b0f.us-south.apigw.appdomain.cloud/dealerships/dealer-get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)        
+        #return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
